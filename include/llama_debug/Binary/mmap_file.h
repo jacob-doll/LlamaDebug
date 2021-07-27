4eb25c28ae03abff6775d613f903764ed37d8a05
Jacob Doll <99jdoll@gmail.com>
2021-07-26 21:33:08 -0400

    Begin adding support for memory mapped files.

---
+++ include/llama_debug/Binary/mmap_file.h
@@ -0,0 +1,37 @@
+#ifndef LLAMADEBUG_MMAP_FILE_H
+#define LLAMADEBUG_MMAP_FILE_H
+
+#include <string>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif /* _WIN32 */
+
+namespace llama_debug {
+
+class mmap_file
+{
+public:
+  mmap_file(const std::string &filename);
+  
+  void close();
+
+  uint8_t *ptr() { return m_ptr; }
+  uint32_t size() { return m_size; }
+
+private:
+  bool init(const std::string &filename);
+
+private:
+#ifdef _WIN32
+  HANDLE m_file;
+  HANDLE m_map_file;
+#endif
+  uint8_t *m_ptr;
+  uint32_t m_size;
+};
+
+}// namespace llama_debug
+
+#endif// LLAMADEBUG_MMAP_FILE_H
---
+++ src/Binary/mmap_file.cpp
@@ -0,0 +1,46 @@
+#include <llama_debug/binary/mmap_file.h>
+
+namespace llama_debug {
+
+mmap_file::mmap_file(const std::string &filename)
+{
+  if (!init(filename)) {
+    throw "Could not map file!";
+  }
+}
+
+bool mmap_file::init(const std::string &filename)
+{
+#ifdef _WIN32
+  m_file = CreateFile(filename.c_str(), GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+  if (m_file == INVALID_HANDLE_VALUE) {
+    return false;
+  }
+
+  m_map_file = CreateFileMapping(m_file, NULL, PAGE_READONLY, 0, 0, NULL);
+  if (m_map_file == 0) {
+    CloseHandle(m_file);
+    return false;
+  }
+
+  m_ptr = (uint8_t *)MapViewOfFile(m_map_file, FILE_MAP_READ, 0, 0, 0);
+  if (m_ptr == 0) {
+    CloseHandle(m_file);
+    CloseHandle(m_map_file);
+    return false;
+  }
+  return true;
+#endif
+  return false;
+}
+
+void mmap_file::close()
+{
+#ifdef _WIN32
+  UnmapViewOfFile((LPVOID)m_ptr);
+  CloseHandle(m_map_file);
+  CloseHandle(m_file);
+#endif
+}
+
+}// namespace llama_debug
