85250a1cfc0a7e742b77d715765fd59717efec10
Jacob Doll <99jdoll@gmail.com>
2021-09-01 09:44:11 -0400

    began pipe abstraction

---
+++ include/llama_debug/process/pipe.h
@@ -0,0 +1,15 @@
+#ifndef LLAMADEBUG_PIPE_H
+#define LLAMADEBUG_PIPE_H
+
+namespace llama_debug {
+
+class pipe
+{
+public:
+  pipe();
+};
+
+}// namespace llama_debug
+
+
+#endif// LLAMADEBUG_PIPE_H
