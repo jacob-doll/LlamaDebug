e7fb86ca4eaac14626e162e1f5e4a8619feec2e5
Jacob Doll <jacob.g.doll@gmail.com>
2021-09-10 12:24:10 -0400

    Backtracked on idea i had

--- include/llama_debug/binary/parser.h
+++ include/llama_debug/binary/parser.h
@@ -6,13 +6,11 @@
 #include <memory>
 
 #include "llama_debug/binary/binary.h"
-#include "llama_debug/process/process.h"
 
 namespace llama_debug {
 
 std::unique_ptr<binary> parse(const std::string &filename);
 std::unique_ptr<binary> parse(const uint8_t *buffer, const uint32_t size);
-std::unique_ptr<binary> parse(const process &proc);
 
 }// namespace llama_debug
 
